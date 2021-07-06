a6f44a25c14b95b1acbb5666dbc34487a4dc2ce5
Minh Nguyen Quang <admin@Admins-Mac-mini.local>
2021-07-06 18:01:47 +0700

    To do list useState and Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.scss";
-import ColorBox from "./Components/ColorBox/index";
+import TodoList from "./Components/TodoList/index";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Reading Book" },
+    { id: 2, title: "Play football" },
+    { id: 3, title: "Play chess" },
+  ]);
+
+  function handleTodo(todo) {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Wellcome to React Hooks</h1>
-      <ColorBox />
+      <h1>React Hooks todolist</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodo} />
     </div>
   );
 }
