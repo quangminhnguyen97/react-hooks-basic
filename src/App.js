c3b7946dea57f61a9b68b50273c75937a38f64f2
Minh Nguyen Quang <admin@Admins-Mac-mini.local>
2021-07-06 16:00:38 +0700

    Use State EX1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import ColorBox from "./Components/ColorBox/index";
 
 function App() {
   return (
     <div className="app">
       <h1>Wellcome to React Hooks</h1>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/Components/ColorBox/index.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import './ColorBox.scss';
+import PropTypes from 'prop-types';
+
+ColorBox.propTypes = {
+    
+};
+
+function getRandomColor() {
+    const LIST_COLOR = ['red', 'green', 'blue', 'orange', 'deeppink'];
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return LIST_COLOR[randomIndex]
+}
+
+function ColorBox() {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        return initColor
+    });
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor)
+        localStorage.setItem('box_color', newColor)
+    }
+
+    return (
+        <div className="ColorBox"
+            style={{backgroundColor: color}}
+            onClick={handleBoxClick}
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
